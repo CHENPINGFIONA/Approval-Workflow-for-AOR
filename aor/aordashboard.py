b025bcd43008be9cd9b6329392b0f1dd9e221590
FIONA\Fiona <chen_ping2010@hotmail.com>
2024-08-29 11:08:04 +0800

    add dashboard

--- aor/aordashboard.py
+++ aor/aordashboard.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from PIL import Image
+import os
+cwd = os.getcwd()
 
 def prototype_application():
     # Display image from a URL
-    st.image("https://your-image-url.com/image.jpg", caption='Image Caption', use_column_width=True)
+    UPLOAD_DIRECTORY = os.path.join(cwd, 'resources\\')
+    # Load the image from a local file
+    image = Image.open(UPLOAD_DIRECTORY+"aordashboard.jpg")  # Replace with your image file path
+    # Display the image
+    st.image(image,  use_column_width=True)
